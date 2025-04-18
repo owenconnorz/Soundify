61ee18edb8afba31dd4c0c57f4dc994bb6a5b560
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 00:26:33 +0100

    Update ReleaseNotesCard.kt

--- app/src/main/java/com/metrolist/music/ui/component/ReleaseNotesCard.kt
+++ app/src/main/java/com/metrolist/music/ui/component/ReleaseNotesCard.kt
@@ -1,4 +1,4 @@
-package com.metrolist.music.ui.component
+package com.soundify.music.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,7 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.metrolist.music.R
+import com.soundify.music.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
