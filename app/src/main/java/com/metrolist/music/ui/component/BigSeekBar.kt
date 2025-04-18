5ac71601c6ab1108280848e19b38d98d27260e69
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 00:11:40 +0100

    Update BigSeekBar.kt

--- app/src/main/java/com/metrolist/music/ui/component/BigSeekBar.kt
+++ app/src/main/java/com/metrolist/music/ui/component/BigSeekBar.kt
@@ -1,4 +1,4 @@
-package com.metrolist.music.ui.component
+package com.soundify.music.ui.component
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
