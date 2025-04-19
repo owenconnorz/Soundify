4b19c77eaba17b5081c0a702dbd69ec0eff0da6c
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 03:52:53 +0100

    fixes

--- app/src/main/java/com/soundify/music/ui/theme/Theme.kt
+++ app/src/main/java/com/soundify/music/ui/theme/Theme.kt
@@ -24,7 +24,7 @@ import com.google.material.color.score.Score
 val DefaultThemeColor = Color(0xFFED5564)
 
 @Composable
-fun MetrolistTheme(
+fun SoundifyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     pureBlack: Boolean = false,
     themeColor: Color = DefaultThemeColor,
