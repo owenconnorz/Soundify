82060f98552e060fcd68078d626f43efad857183
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 19:03:48 +0100

    test 10000

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,27 +55,6 @@ android {
         }
     }
 
-    signingConfigs {
-        create("persistentDebug") {
-            storeFile = file("persistent-debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-        create("release") {
-            storeFile = file("keystore/release.keystore")
-            storePassword = System.getenv("STORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
-        }
-        getByName("debug") {
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-            storePassword = "android"
-            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = true
@@ -88,7 +67,6 @@ android {
         debug {
             applicationIdSuffix = ".debug"
             isDebuggable = true
-            signingConfig = signingConfigs.getByName("persistentDebug")
         }
     }
 
