593e9a05ae06fd18f4e48d3bbf4cf57129f18467
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 02:00:48 +0100

    Update Queue.kt

--- app/src/main/java/com/metrolist/music/playback/queues/Queue.kt
+++ app/src/main/java/com/metrolist/music/playback/queues/Queue.kt
@@ -1,8 +1,8 @@
-package com.metrolist.music.playback.queues
+package com.soundify.music.playback.queues
 
 import androidx.media3.common.MediaItem
-import com.metrolist.music.extensions.metadata
-import com.metrolist.music.models.MediaMetadata
+import com.soundify.music.extensions.metadata
+import com.soundify.music.models.MediaMetadata
 
 interface Queue {
     val preloadItem: MediaMetadata?
