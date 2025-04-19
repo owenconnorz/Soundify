2013935a7f254261b12c90cdb0799333967eaf46
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 01:59:20 +0100

    Update ListQueue.kt

--- app/src/main/java/com/metrolist/music/playback/queues/ListQueue.kt
+++ app/src/main/java/com/metrolist/music/playback/queues/ListQueue.kt
@@ -1,7 +1,7 @@
-package com.metrolist.music.playback.queues
+package com.soundify.music.playback.queues
 
 import androidx.media3.common.MediaItem
-import com.metrolist.music.models.MediaMetadata
+import com.soundify.music.models.MediaMetadata
 
 class ListQueue(
     val title: String? = null,
