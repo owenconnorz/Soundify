8dc3776e200362fd9ca8b5e3984953d4e8bb3321
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 15:19:32 +0100

    Update GetSearchSuggestionsResponse.kt

--- innertube/src/main/java/com/metrolist/innertube/models/response/GetSearchSuggestionsResponse.kt
+++ innertube/src/main/java/com/metrolist/innertube/models/response/GetSearchSuggestionsResponse.kt
@@ -1,6 +1,6 @@
-package com.metrolist.innertube.models.response
+package com.soundify.innertube.models.response
 
-import com.metrolist.innertube.models.SearchSuggestionsSectionRenderer
+import com.soundify.innertube.models.SearchSuggestionsSectionRenderer
 import kotlinx.serialization.Serializable
 
 @Serializable
