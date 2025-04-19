b288e67acc7159a46075eeeaf9b60bba5eeaa165
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 03:25:26 +0100

    gixes

--- app/src/main/java/com/metrolist/music/utils/Utils.kt
+++
@@ -1,5 +0,0 @@
-package com.soundify.music.utils
-
-fun reportException(throwable: Throwable) {
-    throwable.printStackTrace()
-}
--- app/src/main/java/com/metrolist/music/utils/potoken/PoTokenResult.kt
+++
@@ -1,6 +0,0 @@
-package com.soundify.music.utils.potoken
-
-class PoTokenResult(
-    val playerRequestPoToken: String,
-    val streamingDataPoToken: String,
-)
