d41ec673e73766b283913ba47a92c5971c7c08e9
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-19 14:43:27 +0100

    Update LibraryContinuationPage.kt

--- innertube/src/main/java/com/metrolist/innertube/pages/LibraryContinuationPage.kt
+++ innertube/src/main/java/com/metrolist/innertube/pages/LibraryContinuationPage.kt
@@ -1,6 +1,6 @@
-package com.metrolist.innertube.pages
+package com.soundify.innertube.pages
 
-import com.metrolist.innertube.models.YTItem
+import com.soundify.innertube.models.YTItem
 
 data class LibraryContinuationPage(
     val items: List<YTItem>,
