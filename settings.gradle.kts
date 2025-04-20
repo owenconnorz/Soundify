da35758bf756aa232ae4665e5660ed778dda400c
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-20 13:51:38 +0100

    fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,15 +10,20 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Soundify"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+}
+
+rootProject.name = "Metrolist"
 include(":app")
 include(":innertube")
 include(":kugou")
 include(":lrclib")
+include(":kizzy")
 include(":material-color-utilities")
 
 // Use a local copy of NewPipe Extractor by uncommenting the lines below.
-// We assume, that Soundify and NewPipe Extractor have the same parent directory.
+// We assume, that Metrolist and NewPipe Extractor have the same parent directory.
 // If this is not the case, please change the path in includeBuild().
 //
 // For this to work you also need to change the implementation in innertube/build.gradle.kts
@@ -27,20 +32,8 @@ include(":material-color-utilities")
 //      implementation(libs.newpipe.extractor)
 // To:
 //      implementation("com.github.teamnewpipe:NewPipeExtractor")
-
 //includeBuild("../NewPipeExtractor") {
 //    dependencySubstitution {
 //        substitute(module("com.github.teamnewpipe:NewPipeExtractor")).using(project(":extractor"))
-//    }
-//}
-
-
-// Use a local copy of taglib by uncommenting the lines below.
-// We assume, that Soundify and taglib have the same parent directory.
-//
-// Use a local copy of taglib
-//includeBuild("../taglib") {
-//    dependencySubstitution {
-//        substitute(module("com.kyant:taglib"))
 //    }
 //}
