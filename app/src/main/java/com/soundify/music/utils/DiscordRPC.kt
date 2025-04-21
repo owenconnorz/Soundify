8498327624b2105acb3b283519070ae401abb673
Owen <96526481+owencz1998@users.noreply.github.com>
2025-04-21 08:44:39 +0100

    Update DiscordRPC.kt

--- app/src/main/java/com/soundify/music/utils/DiscordRPC.kt
+++ app/src/main/java/com/soundify/music/utils/DiscordRPC.kt
@@ -21,7 +21,7 @@ class DiscordRPC(
             smallText = song.artists.firstOrNull()?.name,
             buttons = listOf(
                 "Listen on YouTube Music" to "https://music.youtube.com/watch?v=${song.song.id}",
-                "Visit Metrolist" to "https://github.com/mostafaalagamy/Metrolist"
+                "Visit Soundify" to "https://github.com/pwencz1998/Soundify"
             ),
             type = Type.LISTENING,
             since = System.currentTimeMillis(),
